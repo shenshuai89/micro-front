5422085c4987b31a9e2bfe2b222026ee4b588c08
申帅 <shuai.shen@ersoft.cn>
2022-04-25 20:52:27 +0800

    feat: qiankun框架的react子应用设置

--- qiankun-demo/module-react/src/App.js
+++ qiankun-demo/module-react/src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
-
+import { BrowserRouter, Route, Link } from "react-router-dom";
+// react子应用的路由都以/react 开头
+const BASE_NAME = window.__POWERED_BY_QIANKUN__ ? "/react" : "/";
 function App() {
+  console.log("BASE_NAME", BASE_NAME);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <BrowserRouter basename={BASE_NAME}>
+      <Link to="/">首页 </Link> | 
+      <Link to="/about"> 关于</Link>
+      <Route path="/" exact render={()=><Home />}></Route>
+      <Route path="/about" render={()=><About />}></Route>
+    </BrowserRouter>
   );
+}
+
+function Home() {
+  return <div>Welcome home </div>;
+}
+
+function About() {
+  return <div>Welcome about </div>;
 }
 
 export default App;
--- qiankun-demo/module-react/src/index.js
+++ qiankun-demo/module-react/src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+if (window.__POWERED_BY_QIANKUN__) {
+}
 
+export async function bootstrap() {}
+export async function mount(props) {
+  render();
+}
+export async function unmount() {}
