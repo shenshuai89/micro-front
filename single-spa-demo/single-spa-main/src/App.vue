584d2d6aff43e4eeac363250db384d55134274d0
申帅 <shuai.shen@ersoft.cn>
2022-04-25 20:34:55 +0800

    feat: single-spa主应用设置

--- single-spa-demo/single-spa-main/src/App.vue
+++ single-spa-demo/single-spa-main/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/vue">Vue</router-link> |
+    <router-link to="/react">React</router-link>
   </div>
   <router-view/>
 </template>
--- single-spa-demo/single-spa-main/src/main.js
+++ single-spa-demo/single-spa-main/src/main.js
@@ -1,5 +1,40 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { registerApplication, start } from "single-spa";
+import router from "./router"
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
+
+// 动态加载js文件
+function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.append(script);
+  });
+}
+// singleSpa 缺陷：样式不隔离，不能动态加载js，没有js沙箱机制
+registerApplication(
+  "myVueApp",
+  async () => {
+      console.log('object');
+    await loadScript("http://localhost:8001/js/chunk-vendors.js");
+    await loadScript("http://localhost:8001/js/app.js");
+    return window.singleVue;
+  },
+  (location) => location.pathname.startsWith("/vue")
+);
+
+registerApplication(
+  "myReactApp",
+  async () => {
+    await loadScript("http://localhost:8002/static/js/bundle.js");
+    return window.singleReact;
+  },
+  (location) => location.pathname.startsWith("/react")
+);
+
+start();
